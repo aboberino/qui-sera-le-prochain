a9c54761da0959e5424ad45d63b205c11dd83497
aboberino <aim1.boban@gmail.com>
2023-09-28 19:25:25 +0200

    intial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Qui sera le prochain
+
+Pronostique qui sera le prochain membre de ton équipe à déguerpir.
+
+## Stack
+
+- ViteJS
+- ReactJS
+- Typescript
+- Pocketbase
+
+## Dev
+
+First use
+
+    npm install
+
+Front: http://localhost:5173
+
+    npm run dev:front
+
+Back: http://localhost:8090
+
+    npm run dev:back
---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Home from './pages/home'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import 'remixicon/fonts/remixicon.css'
+import PocketBase from 'pocketbase'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './pages/login'
+
+const router = createBrowserRouter([
+    { path: '/', element: <Home /> },
+    { path: '/login', element: <Login /> }
+])
+
+const queryClient = new QueryClient()
+export const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL)
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    </React.StrictMode>
+)
